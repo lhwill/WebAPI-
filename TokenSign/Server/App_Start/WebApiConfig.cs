b87e9a22d3bca5d7bd882da8d2c5f358dfa17e95
hao liang <lhwill@foxmail.com>
2020-03-13 11:17:47 +0800

    webapi安全

---
+++ TokenSign/Server/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace Server
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // WebAPI映射路由
+            config.MapHttpAttributeRoutes();
+            //WebAPI增加行为过滤器
+            config.Filters.Add(new Filters.ApiSecurityFilter());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ TokenSign/Server/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Server.Common;
+using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Server.Controllers
+{
+    public class ProductController : ApiController
+    {
+        /// <summary>
+        /// 根据ID查询商品
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public HttpResponseMessage GetProduct(string id)
+        {
+            var product = new Product() { Id = 1, Name = "哇哈哈", Count = 10, Price = 38.8 };
+            ResultMsg resultMsg = null;
+            resultMsg = new ResultMsg();
+            resultMsg.StatusCode = (int)StatusCodeEnum.Success;
+            resultMsg.Info = StatusCodeEnum.Success.GetEnumText();
+            resultMsg.Data = product;
+            return HttpResponseExtension.toJson(JsonConvert.SerializeObject(resultMsg));
+        }
+
+        /// <summary>
+        /// 添加商品
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public HttpResponseMessage AddProudct(Product product)
+        {
+            ResultMsg resultMsg = null;
+            resultMsg = new ResultMsg();
+            resultMsg.StatusCode = (int)StatusCodeEnum.Success;
+            resultMsg.Info = StatusCodeEnum.Success.GetEnumText();
+            resultMsg.Data = product;
+            return HttpResponseExtension.toJson(JsonConvert.SerializeObject(resultMsg));
+        }
+    }
+}
